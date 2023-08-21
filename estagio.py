3391b8f08e87812c9fdb0b532ff4aa9de6e69452
jvalmeida1810 <joaovitoralmeida1100@hotmail.com>
2023-08-21 17:28:34 -0300

    upando script ao git

--- estagio.py
+++ estagio.py
@@ -1,15 +1,29 @@
-from PyPDF2 import PdfReader
+import fitz
+import json
 
-def extrair_texto_pdf(acesso_pdf):
-    reader = PdfReader(acesso_pdf)
-    texto = ''
+def extrair_informacoes_pdf(pdf_path):
+    pdf_document = fitz.open(pdf_path)
+    informacoes = []
+    print(f"Total de páginas: {pdf_document.page_count}")
+    
+    for page_num in range(pdf_document.page_count):
+        print(f"Entrou pela {page_num}° vez")
+        page = pdf_document.load_page(page_num)
+        page_text: str = page.get_text("text")
+        lines_text = page_text.splitlines()
+        document_user = {}
+        for line in lines_text:
+            if ':' in line:
+                split_data = line.split(':')
+                document_user[split_data[0]] = split_data[1] if len(split_data) > 1 else ''
+        informacoes.append(document_user)
 
-    for pagina_num in range(len(reader.pages)):
-        pagina = reader.pages[pagina_num]
-        texto += pagina.extract_text()
+    pdf_document.close()
+    print(f"Retornou: {informacoes}")
+    return informacoes
 
-    return texto
+pdf_path = "Acesso.pdf"
+informacoes_extraidas = extrair_informacoes_pdf(pdf_path)
 
-acesso_pdf = 'Acesso.pdf'
-texto_extraido = extrair_texto_pdf(acesso_pdf)
-print(texto_extraido)
+with open('result.json', 'w', encoding='utf-8') as file:
+    json.dump(informacoes_extraidas, file, indent=2, ensure_ascii=False)
