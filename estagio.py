0bb295ed093c8541bf24f86a782f67834b647235
jvalmeida1810 <joaovitoralmeida1100@hotmail.com>
2023-08-24 13:58:05 -0300

    upando 2

--- estagio.py
+++ estagio.py
@@ -6,18 +6,24 @@ def extrair_informacoes_pdf(pdf_path):
     informacoes = []
     print(f"Total de páginas: {pdf_document.page_count}")
     
-    for page_num in range(pdf_document.page_count):
+    
+    page_num = 1
+    while page_num < pdf_document.page_count:
+        
         print(f"Entrou pela {page_num}° vez")
         page = pdf_document.load_page(page_num)
         page_text: str = page.get_text("text")
         lines_text = page_text.splitlines()
+        
         document_user = {}
         for line in lines_text:
             if ':' in line:
+                
                 split_data = line.split(':')
                 document_user[split_data[0]] = split_data[1] if len(split_data) > 1 else ''
+                print(type(document_user))
         informacoes.append(document_user)
-
+        page_num +=1
     pdf_document.close()
     print(f"Retornou: {informacoes}")
     return informacoes
